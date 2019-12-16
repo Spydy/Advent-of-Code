5d18d0bd1b286bd67cfb5e79f891f6c3f740e7ac
Spydy <Spydymies@gmail.com>
2019-12-16 17:57:53 +0200

    advent 4 1st star

---
+++ 4/advent_4.py
@@ -0,0 +1,30 @@
+def first_star():
+    password_range = (278384, 824795)
+
+    valid_password_count = 0
+
+    def check_valid(password):
+        string_password = str(password)
+        double_found = False
+        for number in range(1, 6):
+            if int(string_password[number]) < int(string_password[number-1]):
+                return False
+            if int(string_password[number]) == int(string_password[number-1]):
+                double_found = True
+        if double_found:
+            return True
+
+    for password in range(password_range[0], password_range[1]+1):
+        if not check_valid(password):
+            continue
+
+        valid_password_count += 1
+
+    print(valid_password_count)
+
+def second_star():
+
+
+if __name__ == "__main__":
+    first_star()
+    second_star()
