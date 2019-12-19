be9770f7dfdbd58a73dc67632c98e31a0f76e840
Spydy <Spydymies@gmail.com>
2019-12-19 21:20:42 +0200

    Advent 5 1st start

---
+++ 5/advent_5.py
@@ -0,0 +1,115 @@
+from enum import Enum
+
+
+Action = {
+    "ADD": ["01", "1"],
+    "MULTIPLY": ["02", "2"],
+    "INPUT": ["03", "3"],
+    "OUTPUT": ["04", "4"],
+    "STOP": "99"
+}
+
+ParameterMode = {
+    "POSITION": "0",
+    "IMMEDIATE": "1"
+}
+
+
+def intcode_computer(intcode):
+    intcode = intcode.split(",")
+    index = 0
+
+    while True:
+        parameter = str(intcode[index])
+
+        if len(parameter) > 2:
+            opcode = parameter[-2:]
+            parameter_modes = parameter[:-2]
+        else:
+            opcode = parameter
+            parameter_modes = ''
+
+        if opcode in Action["STOP"]:
+            break
+
+        elif opcode in Action["ADD"]:
+            try:
+                noun_parameter_mode = parameter_modes[-1]
+            except IndexError:
+                noun_parameter_mode = "0"
+            try:
+                verb_parameter_mode = parameter_modes[-2]
+            except IndexError:
+                verb_parameter_mode = "0"
+
+            if noun_parameter_mode in ParameterMode["POSITION"]:
+                noun = int(intcode[int(intcode[index+1])])
+            elif noun_parameter_mode in ParameterMode["IMMEDIATE"]:
+                noun = int(intcode[index+1])
+
+            if verb_parameter_mode in ParameterMode["POSITION"]:
+                verb = int(intcode[int(intcode[index+2])])
+            elif verb_parameter_mode in ParameterMode["IMMEDIATE"]:
+                verb = int(intcode[index+2])
+
+            output_position = int(intcode[index+3])
+
+            intcode[output_position] = str(noun + verb)
+
+            index = index+4
+
+        elif opcode in Action["MULTIPLY"]:
+            try:
+                noun_parameter_mode = parameter_modes[-1]
+            except IndexError:
+                noun_parameter_mode = "0"
+            try:
+                verb_parameter_mode = parameter_modes[-2]
+            except IndexError:
+                verb_parameter_mode = "0"
+
+            if noun_parameter_mode == ParameterMode["POSITION"]:
+                noun = int(intcode[int(intcode[index + 1])])
+            elif noun_parameter_mode == ParameterMode["IMMEDIATE"]:
+                noun = int(intcode[index + 1])
+
+            if verb_parameter_mode == ParameterMode["POSITION"]:
+                verb = int(intcode[int(intcode[index + 2])])
+            elif verb_parameter_mode == ParameterMode["IMMEDIATE"]:
+                verb = int(intcode[index + 2])
+
+            output_position = int(intcode[index + 3])
+
+            intcode[output_position] = str(noun * verb)
+
+            index = index + 4
+
+        elif opcode in Action["INPUT"]:
+            output_position = int(intcode[index + 1])
+            print("Please give input: ")
+            user_input = input()
+            intcode[output_position] = str(user_input)
+
+            index = index + 2
+
+
+        elif opcode in Action["OUTPUT"]:
+            try:
+                noun_parameter_mode = parameter_modes[-1]
+            except IndexError:
+                noun_parameter_mode = "0"
+
+            if noun_parameter_mode == ParameterMode["POSITION"]:
+                output_position = int(intcode[index + 1])
+                print("OUTPUT: " + intcode[output_position])
+            elif noun_parameter_mode == ParameterMode["IMMEDIATE"]:
+                print("OUTPUT: " + intcode[index + 1])
+
+            index = index + 2
+
+        else:
+            index = index + 1
+
+with open("input.txt", "r") as input_file:
+    file_intcode = input_file.readline()
+    intcode_computer(file_intcode)
